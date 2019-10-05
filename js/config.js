75b4c512b69d21257da1817b266a1511f9dfc895
Quique <ncnr_quique@hotmail.com>
2019-10-05 16:54:30 -0500

    .

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_PdNSnrLQB', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2n2f48napajl04a77anavs3cvk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '43tj900c7ln01udjeq5jhtvf7s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
